8152ad2b583f50634bb4e245af189b5b3c22a90a
Ishaan Kurmi <66128279+Ishaan149@users.noreply.github.com>
2025-02-06 18:26:59 -0700

    Changed for errors

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,14 +11,14 @@ const About = () => {
                     <p className="text-base lg:text-lg">
                     Hi! I'm Ishaan Kurmi, a Computer Science student at Arizona State University with a passion for software development, data analysis, and machine learning. I love solving problems and building full-stack applications, predictive models, and iOS apps that turn ideas into impactful solutions.<br/>
 
-                    <br/>I’m particularly fascinated by machine learning and its potential to drive innovation, automate decisions, and uncover hidden insights. From training deep learning models to optimizing algorithms, I enjoy experimenting with AI-driven solutions that solve real-world problems efficiently.<br/>
+                    <br/>I'm particularly fascinated by machine learning and its potential to drive innovation, automate decisions, and uncover hidden insights. From training deep learning models to optimizing algorithms, I enjoy experimenting with AI-driven solutions that solve real-world problems efficiently.<br/>
 
-                    <br/>I’m always eager to learn, innovate, and take on new challenges. Let’s connect on GitHub and LinkedIn to create something amazing!
+                    <br/>I'm always eager to learn, innovate, and take on new challenges. Let's connect on GitHub and LinkedIn to create something amazing!
                     </p>
                 </div>
             </div>
 
-            <img className='mx-auto rounded-3xl py-8 md:py-0' src={aboutImg} width={500} height={300}/>
+            <img className='mx-auto rounded-3xl py-8 md:py-0' src={aboutImg} width={500} height={300} alt=''/>
         </div>
     </div>
   )
