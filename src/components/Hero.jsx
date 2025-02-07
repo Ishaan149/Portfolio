c1d90873ba61075c61d5832f9779133a5903a2e9
Ishaan Kurmi <66128279+Ishaan149@users.noreply.github.com>
2025-02-07 12:54:14 -0700

    Changed Download CV button for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,29 +34,35 @@ const Hero = () => {
             My name is Ishaan Kurmi, a Computer Science student at ASU.
             </p>
 
-            <div className='my-8'>
-                <a href='https://drive.google.com/uc?export=download&id=1dA7I8GGzdf4PJ4EpDiOeQh69IT0Xc8HY
-                    ' className='px-6 py-3 w-full rounded-xl mr-4 
-                                    bg-gradient-to-br from-[#ea98da] to-[#5b6cf9] text-white'>
-                    Download cv
+            <div className='my-8 flex flex-col sm:flex-row items-center sm:items-start gap-4'>
+                <a 
+                    href='https://drive.google.com/uc?export=download&id=1dA7I8GGzdf4PJ4EpDiOeQh69IT0Xc8HY'
+                    className='px-6 py-3 rounded-xl bg-gradient-to-br from-[#ea98da] to-[#5b6cf9] text-white 
+                        w-full sm:w-auto block sm:inline-block text-center sm:mr-4'>
+                    Download CV
                 </a>
 
-                <a href='https://github.com/Ishaan149' 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className='px-6 py-3 w-full rounded-xl mr-4 border border-gray-400 hover:bg-gradient-to-br from-[#ea98da] to-[#5b6cf9] text-white hover:border-none'>
-                        
-                        GitHub
+
+                <a 
+                    href='https://github.com/Ishaan149' 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className='px-6 py-3 w-full sm:w-auto rounded-xl border border-gray-400 
+                            hover:bg-gradient-to-br from-[#ea98da] to-[#5b6cf9] text-white 
+                            hover:border-none text-center'>
+                    GitHub
                 </a>
 
-                <a href='https://www.linkedin.com/in/ishaan-kurmi-35ab92290'
-                target="_blank" 
-                rel="noopener noreferrer" 
-                className='px-6 py-3 w-full rounded-xl mr-4 border border-gray-400 hover:bg-gradient-to-br from-[#ea98da] to-[#5b6cf9] text-white hover:border-none'>
-                        LinkedIn
+                <a 
+                    href='https://www.linkedin.com/in/ishaan-kurmi-35ab92290'
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className='px-6 py-3 w-full sm:w-auto rounded-xl border border-gray-400 
+                            hover:bg-gradient-to-br from-[#ea98da] to-[#5b6cf9] text-white 
+                            hover:border-none text-center'>
+                    LinkedIn
                 </a>
             </div>
-
         </div>
     </div>
   )
